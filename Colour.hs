f86af62a2502c7b622205e10e0e140bb088a4423
Mitchell Kember <mk12360@gmail.com>
2012-07-05 16:21:22 -0400

    VectorT instead of Vector3D, Vector instead of XYZ; Colour is a vector in colour-space.

--- Colour.hs
+++ Colour.hs
@@ -2,28 +2,18 @@
 
 module Colour
 ( Colour
-, RGB(..)
+, Colour24
 , sRGB24
 ) where
 
-import Control.Applicative (Applicative, pure, (<$>), (<*>))
-import Data.Monoid (Monoid, mappend, mempty)
 import Data.Word (Word8)
 
-type Colour = RGB Double
+import Vector (VectorT)
 
-data RGB a = RGB !a !a !a deriving (Eq, Show)
+type Colour   = VectorT Double
+type Colour24 = VectorT Word8
 
-instance Functor RGB where
-    fmap f (RGB r g b) = RGB (f r) (f g) (f b)
-
-instance Applicative RGB where
-    pure x = RGB x x x
-    (RGB f g h) <*> (RGB x y z) = RGB (f x) (g y) (h z)
-
-instance (Num a) => Monoid (RGB a) where
-    mempty  = RGB 0 0 0
-    mappend = (<*>) . (fmap (+))
+type ColourT  = VectorT
 
 -- Convert a value (colour component) from linear RGB space to sRGB.
 sRGB :: (Floating a, Ord a) => a -> a
@@ -31,5 +21,5 @@ sRGB x | x <= 0.0031308 = 12.92 * x
        | otherwise      = 1.055 * (x ** (1 / 2.4)) - 0.055
 
 -- Convert a linear RGB colour to a 24-bit sRGB colour.
-sRGB24 :: (RealFloat a) => RGB a -> RGB Word8
+sRGB24 :: (RealFloat a) => ColourT a -> Colour24
 sRGB24 = fmap (round . (* 255) . sRGB)
--- Vector.hs
+++ Vector.hs
@@ -3,7 +3,7 @@
 module Vector
 ( Scalar
 , Vector
-, Vector3D(..)
+, VectorT(..)
 , (<+>)
 , (<->)
 , (*>)
@@ -24,71 +24,71 @@ import Control.Monad.Instances
 import Data.Monoid (Monoid, mappend, mempty)
 
 type Scalar = Double
-type Vector = Vector3D Scalar
+type Vector = VectorT Scalar
 
-data Vector3D a = XYZ !a !a !a deriving(Eq, Show)
+data VectorT a = Vector !a !a !a deriving(Eq, Show)
 
-instance Functor Vector3D where
-   fmap f (XYZ x y z) = XYZ (f x) (f y) (f z)
+instance Functor VectorT where
+   fmap f (Vector x y z) = Vector (f x) (f y) (f z)
 
-instance Applicative Vector3D where
-    pure x = XYZ x x x
-    (XYZ f g h) <*> (XYZ x y z) = XYZ (f x) (g y) (h z)
+instance Applicative VectorT where
+    pure x = Vector x x x
+    (Vector f g h) <*> (Vector x y z) = Vector (f x) (g y) (h z)
 
-instance (Num a) => Monoid (Vector3D a) where
+instance (Num a) => Monoid (VectorT a) where
     mempty  = vzero
     mappend = (<*>) . (fmap (+))
 
 -- Calculate the sum of the components of a vector. This is not directly useful
 -- mathematically, but is used to implement other operators.
-vsum :: (Num a) => Vector3D a -> a
-vsum (XYZ x y z) = x + y + z
+vsum :: (Num a) => VectorT a -> a
+vsum (Vector x y z) = x + y + z
 
 -- Add and subtract two vectors component-wise.
 infixl 6 <+>, <->
-(<+>), (<->) :: (Num a) => Vector3D a -> Vector3D a -> Vector3D a
+(<+>), (<->) :: (Num a) => VectorT a -> VectorT a -> VectorT a
 (<+>) = mappend
 (<->) = (. vnegate) . mappend
 
 -- Scale a vector by a scalar value, that is multiply each component of the
 -- vector by the scalar.
 infixl 8 *>
-(*>) :: (Num a) => a -> Vector3D a -> Vector3D a
+(*>) :: (Num a) => a -> VectorT a -> VectorT a
 (*>) = fmap . (*)
 
 -- Calculate the dot product, that is the scalar product, of two vectors.
 infixl 8 <.>
-(<.>) :: (Num a) => Vector3D a -> Vector3D a -> a
+(<.>) :: (Num a) => VectorT a -> VectorT a -> a
 (<.>) = (vsum .) . ((<*>) . (fmap (*)))
 
 -- Calculate the cross product of two vectors.
-cross :: (Num a) => Vector3D a -> Vector3D a -> Vector3D a
-cross (XYZ x1 y1 z1) (XYZ x2 y2 z2) = XYZ
+cross :: (Num a) => VectorT a -> VectorT a -> VectorT a
+cross (Vector x1 y1 z1) (Vector x2 y2 z2) = Vector
     (y1 * z2 - z1 * y2)
     (z1 * x2 - x1 * z2)
     (x1 * y2 - y1 * x2)
 
 -- Calculate the magnitude, that is the length, of a vector.
-mag :: (Floating a) => Vector3D a -> a
+mag :: (Floating a) => VectorT a -> a
 mag = sqrt . magSqr
 
 -- Calculate the square of a vector's magnitude. This is more efficient as it
 -- skips the square root computation. In particular, when vectors are sorted by
 -- magnitude, it is much more efficient to sort by the squared magnitude.
-magSqr :: (Num a) => Vector3D a -> a
+magSqr :: (Num a) => VectorT a -> a
 magSqr = vsum . fmap (^ 2)
 
 -- Negate a vector, equivalent to multiplying it by -1. The negated vecto has
 -- the same magnitude as the original, but has the opposite direction.
-vnegate :: (Num a) => Vector3D a -> Vector3D a
+vnegate :: (Num a) => VectorT a -> VectorT a
 vnegate = fmap negate
 
 -- Normalize a vector, that is make it's magnitude equal to one.
-normalize :: (Floating a) => Vector3D a -> Vector3D a
+normalize :: (Floating a) => VectorT a -> VectorT a
 normalize = flip (/) . mag >>= fmap
 
-vzero, unitX, unitY, unitZ :: (Num a) => Vector3D a
-vzero = XYZ 0 0 0
-unitX = XYZ 1 0 0
-unitY = XYZ 0 1 0
-unitZ = XYZ 0 0 1
+vzero, unitX, unitY, unitZ :: (Num a) => VectorT a
+vzero = Vector 0 0 0
+unitX = Vector 1 0 0
+unitY = Vector 0 1 0
+unitZ = Vector 0 0 1
