d9fa32deabcbf94b6b4106ef2b78def940389b14
Mitchell Kember <mk12360@gmail.com>
2012-07-09 20:05:06 -0400

    Modified documentation and export list in Colour.

--- Colour.hs
+++ Colour.hs
@@ -3,6 +3,7 @@
 module Colour
 ( Colour
 , Colour24
+, ColourT
 , sRGB24
 ) where
 
@@ -13,14 +14,14 @@ import Vector
 type Colour   = ColourT Double
 type Colour24 = ColourT Word8
 
--- A colour is a vector in a colour space.
+-- An RGB colour, a three-dimensional vector in a colour space.
 type ColourT  = VectorT
 
--- Convert a value (colour component) from linear RGB space to sRGB.
+-- Convert a single value from linear RGB space to sRGB using gamma encoding.
 sRGB :: (Floating a, Ord a) => a -> a
 sRGB x | x <= 0.0031308 = 12.92 * x
-       | otherwise      = 1.055 * (x ** (1 / 2.4)) - 0.055
+       | otherwise      = 1.055 * x ** (1 / 2.4) - 0.055
 
--- Convert a linear RGB colour to a 24-bit sRGB colour.
+-- Convert a floating-point linear RGB colour to a 24-bit sRGB colour.
 sRGB24 :: (RealFloat a) => ColourT a -> Colour24
-sRGB24 = fmap (round . (* 255) . sRGB)
+sRGB24 = fmap $ round . (* 255) . sRGB
