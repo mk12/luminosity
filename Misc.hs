03e0e62ffd871ee5b3ae8f9d7a9f6f9bce764be1
Mitchell Kember <mk12360@gmail.com>
2012-07-09 21:01:17 -0400

    Removed old commented code from Misc.

--- Misc.hs
+++ Misc.hs
@@ -7,15 +7,3 @@ import Data.List (minimumBy)
 maybeMinBy :: (a -> a -> Ordering) -> [a] -> Maybe a
 maybeMinBy _ [] = Nothing
 maybeMinBy f xs = Just $ minimumBy f xs
-
-
--- Render.hs
--- non exanded form or horizontal offset from center for ortho rayForPixel
--- u = resX / resY * (-k + x / resX * scale)
-
-
--- Intersect.hs
-
--- intersection :: Ray -> Surface -> Maybe Vector
--- intersection r s = fmap (`extend` r) $ intersect r s
--- intersection = liftM2 (.) (fmap . flip extend) intersect
