cc39646d24aba4651966024737f0056bc063ea1b
Mitchell Kember <mk12360@gmail.com>
2012-07-10 01:37:28 -0400

    Worked on Renderer.

--- Render.hs
+++ Render.hs
@@ -2,27 +2,37 @@
 
 module Render where
 
-import Data.Binary.Put
-import qualified Data.ByteString.Lazy as BL
+import Control.Applicative ((<$>), (<*>))
+import Control.Monad (forM_)
+import Data.Binary (Binary, Put, get, put)
+import Data.Word (Word8)
+-- import qualified Data.ByteString.Lazy as BL
 
+-- ()
+import Colour
+import Intersect
 import Trace
 import Vector
 
 type Pixel = (Scalar, Scalar)
 
+instance (Binary a) => Binary (VectorT a) where
+    put (Vector x y z) = put x >> put y >> put z
+    get = Vector <$> get <*> get <*> get
+
 -- Calculate the ray to be launched through a given pixel from a camera.
 -- Non-integral pixel coordinates can be used for oversampling.
 rayForPixel :: Camera -> Settings -> Pixel -> Ray
-rayForPixel (Orthographic (Ray c look) up scale) settings (x, y) = Ray
-    (c <+> v *> up <+> u *> normalize (up >< look)) look
+rayForPixel (Orthographic (Ray c look) up scale) settings (x, y)
+    = Ray (c <+> v *> up <+> u *> normalize (up >< look)) look
   where
     resX   = fromIntegral (mResolutionX settings)
     resY   = fromIntegral (mResolutionY settings)
     k      = scale / 2
     v      = k - scale * (y + 0.5) / resY
     u      = -k * resX / resY + scale * (x + 0.5) / resY
-rayForPixel (Perspective (Ray c look) up length) settings (x, y) = Ray
-    start (normalize $ start <-> focus)
+rayForPixel (Perspective (Ray c look) up length) settings (x, y)
+    = Ray start (normalize $ start <-> focus)
   where
     resX   = fromIntegral (mResolutionX settings)
     resY   = fromIntegral (mResolutionY settings)
@@ -31,9 +41,11 @@ rayForPixel (Perspective (Ray c look) up length) settings (x, y) = Ray
     start  = c <+> v *> up <+> u *> normalize (up >< look)
     focus  = c <-> length *> look
 
+-- Render a scene out to a bytestring, each pixel being 24 bits.
 render :: Scene -> Put
-render s = do
-    forM_ pixels (\xy -> putWord24be
+render scene = forM_ rows $ \y -> forM_ cols $ \x ->
+    put . sRGB24 . trace scene . rayForPixel (mCamera scene) ss $ (x, y)
   where
-    (Settings resX resY samples _) = mSettings s
-    pixels = init . zip $ [0..resX] [0..resY]
+    ss   = mSettings scene
+    rows = init [0..(fromIntegral $ mResolutionY ss)]
+    cols = init [0..(fromIntegral $ mResolutionX ss)]
