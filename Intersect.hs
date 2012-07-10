7ff3bd0fdab3c64a640795019437190e0ea09b2b
Mitchell Kember <mk12360@gmail.com>
2012-07-09 21:01:55 -0400

    Changed documentation in Intersect.

--- Intersect.hs
+++ Intersect.hs
@@ -13,25 +13,24 @@ import Data.Maybe (listToMaybe)
 
 import Vector
 
--- A ray is an arrow in euclidean space represented by an origin vector and a
--- direction vector. It is a line which extends infintely in only one direction.
+-- A Euclidean half-line with an initial point (represented by a positon vector)
+-- which extends infinitely in one direction (represented by a unit vector).
 data Ray = Ray Vector Vector deriving (Eq, Show)
 
--- A surface is a simple geometrical object in euclidean space.
+-- A simple geometrical solid in three-dimensional Euclidean space.
 data Surface = Sphere Vector Scalar
              | Plane  Vector Vector
              deriving (Eq, Show)
 
 -- An extremely small value to prevent problems caused by floating point error.
 epsilon :: (Fractional a) => a
-epsilon = 10e-10
+epsilon = 1.0e-10
 
--- Calculate the location of a point on a ray given an offset from its origin.
+-- Calculate the position of a point on a ray given an offset from its origin.
 extend :: Scalar -> Ray -> Vector
 extend t (Ray x d) = x <+> t *> d
 
--- Calculate the normal unit vector of a point on a surface, where both the
--- surface's position and the given point are relative to the same origin.
+-- Calculate the normal unit vector of a point on a surface.
 normal :: Surface -> Vector -> Vector
 normal (Sphere c _) x = normalize (x <-> c)
 normal (Plane  _ n) _ = n
@@ -49,7 +48,7 @@ intersect (Sphere p r) (Ray x d)
     c    = xo <.> xo - r ^ 2
     disc = b ^ 2 - 4 * c
     root = sqrt disc
-    q    | b < 0  = (-root - b) / 2
+    q    | b <  0 = (-root - b) / 2
          | b >= 0 = ( root - b) / 2
     ts   = [q, c / q]
 intersect (Plane p n) (Ray x d)
