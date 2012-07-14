607f0d6ae5c7d7c718b050e446c99e6248edd4c5
Mitchell Kember <mk12360@gmail.com>
2012-07-14 13:38:47 -0400

    Removed commented function in Misc.

--- Misc.hs
+++ Misc.hs
@@ -7,9 +7,3 @@ import Data.List (minimumBy)
 maybeMinBy :: (a -> a -> Ordering) -> [a] -> Maybe a
 maybeMinBy _ [] = Nothing
 maybeMinBy f xs = Just $ minimumBy f xs
-
--- toMaybe b x = if b then Just x else Nothing
---
--- import Control.Monad (guard)
--- mcondition :: (MonadPlus m) => Bool -> a -> m a
--- mcondition b x = guard b >> return x
